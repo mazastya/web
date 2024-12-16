634d0d411a196eae3f0428571a53add576618021
mazastya <amazanova800@gmail.com>
2024-12-16 18:43:02 +0300

    finished 5 lab

--- js/index.js
+++ js/index.js
@@ -1,38 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.menu-toggle');  // Кнопка для открытия меню
-    const menu = document.querySelector('.menu');  // Меню
-    const mainContent = document.querySelector('.main');  // Основной контент
+    const toggleButton = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+    const mainContent = document.querySelector('.main');
 
-    // Проверяем, что кнопка существует, чтобы избежать ошибок
     if (toggleButton) {
         toggleButton.addEventListener('click', function () {
-            // Переключаем класс 'show' на меню, чтобы отображать или скрывать меню
             menu.classList.toggle('show');
 
-            // Добавляем или удаляем класс 'menu-open' для основного контента,
-            // чтобы сдвигать его при открытом меню
             mainContent.classList.toggle('menu-open');
         });
     }
 
-    // Функция для корректного отображения меню при изменении размера экрана
     function updateMenuState() {
         if (window.innerWidth > 768) {
-            // На больших экранах меню всегда показывается, кнопка скрыта
             menu.classList.add('show');
-            mainContent.classList.add('menu-open');  // Сдвигаем контент вниз
-            toggleButton.style.display = 'none';  // Скрываем кнопку меню
+            mainContent.classList.add('menu-open');
+            toggleButton.style.display = 'none';
         } else {
-            // На мобильных устройствах меню скрыто, кнопка отображается
             menu.classList.remove('show');
             mainContent.classList.remove('menu-open');
-            toggleButton.style.display = 'block';  // Показываем кнопку меню
+            toggleButton.style.display = 'block';
         }
     }
 
-    // Инициализация состояния меню при загрузке страницы
     updateMenuState();
 
-    // Обновление состояния меню при изменении размера окна
     window.addEventListener('resize', updateMenuState);
 });
